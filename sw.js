d963fc179205150f052980224fc03028dafd0a32
edgartorres10 <newdiscox@gmail.com>
2026-03-17 20:27:13 -0300

    Add files via upload
    Atualizando feed musical completo

--- sw.js
+++ sw.js
@@ -1,3 +1,12 @@
-self.addEventListener('install', e => {
-  self.skipWaiting();
+self.addEventListener('install', (e) => {
+    console.log('Service Worker instalado');
+});
+
+self.addEventListener('activate', (e) => {
+    console.log('Service Worker ativado');
+});
+
+self.addEventListener('fetch', (event) => {
+    // Opcional: interceptar requests para cache
+    event.respondWith(fetch(event.request));
 });
